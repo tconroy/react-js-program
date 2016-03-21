77c7d0397c3abe96403b972afdb6795f9c0b7de1
Tom Conroy <tom@thomasconroy.net>
2016-03-21 08:47:56 -0400

    testing

--- app/index.js
+++ app/index.js
@@ -1,15 +1,50 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+// var HelloWorld = React.createClass({
+// 	render: function () {
+// 		return (
+// 			<div>Hello {this.props.name}!</div>
+// 		);
+// 	}
+// });
+
+// ReactDOM.render(
+// 	<HelloWorld name="Tom" />,
+// 	document.getElementById('app')
+// );
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-	render: function () {
+var ProfilePic = React.createClass({
+	render: function() {
 		return (
-			<div>Hello {this.props.name}!</div>
+			<img src={'https://photo.fb.com/'+this.props.username} />
 		);
 	}
 });
+var ProfileLink = React.createClass({
+	render: function () {
+		return (
+			<a href={'https://www.fb.com/'+this.props.username}>
+				{this.props.username}
+			</a>
+		);
+	}
+})
+var Avatar = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<ProfilePic username={this.props.username} />
+				<ProfileLink username={this.props.username} />
+			</div>
+		);
+	}
+})
 
 ReactDOM.render(
-	<HelloWorld name="Tom" />,
+	<Avatar username="tylermcginnis" />,
 	document.getElementById('app')
 );
