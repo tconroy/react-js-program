c2e42aa026693c0de5924318283821fa0b97d26d
Tom Conroy <tom@thomasconroy.net>
2016-03-25 11:47:04 -0400

    progress

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home = React.createClass({
+	render: function () {
+		return (
+			<div> hello form home! </div>
+		);
+	}
+})
+
+module.exports = Home;
--- app/index.js
+++ app/index.js
@@ -1,51 +1,8 @@
-var USER_DATA = {
-	name: 'Tom Conroy',
-	username: 'butts',
-	image: 'https://avatars3.githubusercontent.com/u/1609336?v=3&s=460'
-};
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-	render: function () {
-		return (<img src={this.props.imageUrl} style={{height:100, width:100}} />)
-	}
-});
-
-var ProfileLink = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<a href={'https://www.github.com/' + this.props.username}>
-					{this.props.username}
-				</a>
-			</div>
-		);
-	}
-});
-
-var ProfileName = React.createClass({
-	render: function () {
-		return (
-			<div>{this.props.name}</div>
-		);
-	}
-});
-
-var Avatar = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<ProfilePic imageUrl={this.props.user.image} />
-				<ProfileName name={this.props.user.name} />
-				<ProfileLink username={this.props.user.username} />
-			</div>
-		);
-	}
-})
+var routes = require('./config/routes');
 
 ReactDOM.render(
-	<Avatar user={USER_DATA} />,
+	routes,
 	document.getElementById('app')
 );
